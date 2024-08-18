6bc7284a8b320eb47bd786f8362aaa206d8168e1
ravan-de <50665633+ravan-de@users.noreply.github.com>
2024-08-18 18:25:52 +0200

    gruten fgrow
    guten grow

--- Assets/SizeController.cs
+++ Assets/SizeController.cs
@@ -8,18 +8,23 @@ public class SizeController : MonoBehaviour
     public Transform head;
     public LineRenderer lineRender;
     public Vector3 growRate;
+    private List<RaycastHit2D> results;
+    public ContactFilter2D filter;
     void Start()
     {
-        
+        results = new List<RaycastHit2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        col.transform.localPosition = head.localPosition * 0.5f;
-        head.localPosition += growRate * Time.deltaTime;
-        col.size += (Vector2)growRate * Time.deltaTime;
         lineRender.SetPosition(0, head.transform.position);
         lineRender.SetPosition(1, transform.position);
+        if (Physics2D.Raycast(head.transform.position, transform.up, filter, results, 0.5f) == 0)
+        {
+            col.transform.localPosition = head.localPosition * 0.5f;
+            head.localPosition += growRate * Time.deltaTime;
+            col.size += (Vector2)growRate * Time.deltaTime;
+        }
     }
 }
